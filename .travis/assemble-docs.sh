2b8ad164b0715ad38d6b2a287e3f82dc5df8b1c3
Eduardo Trujillo <ed@chromabits.com>
2016-07-04 19:20:56 -0700

    chore(travis): Add travis.yml and related scripts

---
+++ .travis/assemble-docs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Copy haddocks to a separate directory.
+cp -R "$(stack path --local-doc-root)" ../gh-pages
+cp -R "$(stack path --local-hpc-root)" ../gh-pages
+cd ../gh-pages
+
+# Set identity.
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis"
+
+# Add branch.
+git init
+git remote add origin https://${GH_TOKEN}@github.com/{$TRAVIS_REPO_SLUG}.git > /dev/null
+git checkout -B gh-pages
+
+# Push generated files.
+git add .
+git commit -m "Haddocks updated"
+git push origin gh-pages -fq > /dev/null
---
+++ .travis/before-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+mkdir -p ~/.local/bin
+
+export PATH=$HOME/.local/bin:$PATH
+
+curl -L https://www.stackage.org/stack/linux-x86_64 \
+  | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
+
+stack install hscolour
