65355b72aa0e9bf09d0a1ac3fe1a3e5370576ebe
Eduardo Trujillo <ed@chromabits.com>
2016-07-04 19:40:05 -0700

    fix(travis): Add stack setup

--- .travis/before-install.sh
+++ .travis/before-install.sh
@@ -9,4 +9,5 @@ export PATH=$HOME/.local/bin:$PATH
 curl -L https://www.stackage.org/stack/linux-x86_64 \
   | tar xz --wildcards --strip-components=1 -C ~/.local/bin '*/stack'
 
+stack setup
 stack install hscolour
